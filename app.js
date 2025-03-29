1b5f6a865997044afd8c460f4df04083626002be
Nurbek Tilebaliev <nurbektilebaliev@Nurbeks-MacBook-Air.local>
2025-03-29 14:03:49 +0900

    new hw

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-// Задание 1
-const arr = ['apple', 'banana', 'cherry', 'date'];
-for (const fruit of arr) {
-    console.log(fruit);
+// Функция getInfo
+function getInfo({ name, age, city }) {
+    return `Имя: ${name}, Возраст: ${age}, Город: ${city}`;
 }
 
-// Задание 2
-const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-let password = '';
-for (let i = 0; i < 8; i++) {
-    password += chars[Math.floor(Math.random() * chars.length)];
+const user = {
+    name: "Baktybek",
+    age: 18,
+    city: "Bishkek",
+};
+console.log(getInfo(user));
+// "Имя: Baktybek, Возраст: 18, Город: Bishkek"
+
+// Функция mergeArrays
+function mergeArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
 }
-console.log("Сгенерированный пароль:", password);
+console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
+// [1, 2, 3, 4, 5, 6]
 
-// Задание 3
-const person = { name: "Aibek", age: 25, country: "Kyrgyzstan" };
-const { name, age } = person;
-console.log("Имя:", name, "Возраст:", age);
-
-// Задание 4
-const car1 = { brand: "Toyota", year: 2015 };
-const car2 = { brand: "Honda", year: 2020 };
-const newCar = { ...car1, ...car2 };
-console.log("Объединённый объект:", newCar);
-
-// Задание 5
-const values = [10, "hello", 25, "world", 30, false, 50, "JS"];
-const numbers = values.filter(value => typeof value === 'number');
-console.log("Массив с числами:", numbers);
-
+// Функция getFirstAndRest
+function getFirstAndRest([first, ...rest]) {
+    return `Первый элемент: ${first}, Остальные: [${rest.join(", ")}]`;
+}
+console.log(getFirstAndRest([10, 20, 30, 40]));
+// "Первый элемент: 10, Остальные: [20, 30, 40]"
